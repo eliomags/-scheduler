d81c58bd9f056ff544d2b4a84835ea7d4bcad9e2
Elshan Magsudov <elshanmagsudov@Elshans-MacBook-Pro.local>
2023-02-28 17:47:33 +0400

    created a new <Appointment>component with its own directory and an empty component to load styles shared between child views.

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,104 @@
+import React from "react";
+import "components/Appointment/styles.scss";
+
+// import Header from "components/Appointment/Header";
+// import Show from "components/Appointment/Show";
+// import Empty from "components/Appointment/Empty";
+// import Form from "components/Appointment/Form";
+// import Status from "components/Appointment/Status";
+// import Confirm from "components/Appointment/Confirm";
+// import Error from "components/Appointment/Error";
+// import useVisualMode from "hooks/useVisualMode";
+
+// export default function Appointment(props) {
+//     const EMPTY = "EMPTY";
+//     const SHOW = "SHOW";
+//     const CREATE = "CREATE";
+//     const SAVING = "SAVING";
+//     const DELETING = "DELETING";
+//     const CONFIRM = "CONFIRM";
+//     const EDIT = "EDIT";
+//     const ERROR_SAVE = "ERROR_SAVE";
+//     const ERROR_DELETE = "ERROR_DELETE";
+
+//     const { mode, transition, back } = useVisualMode(
+//         props.interview ? SHOW : EMPTY
+//     );
+
+//     function save(name, interviewer) {
+//         const interview = {
+//             student: name,
+//             interviewer
+//         };
+//         transition(SAVING);
+//         props
+//             .bookInterview(props.id, interview)
+//             .then(() => transition(SHOW))
+//             .catch(error => transition(ERROR_SAVE, true));
+//     }
+
+//     function destroy() {
+//         transition(DELETING, true);
+//         props
+//             .cancelInterview(props.id)
+//             .then(() => transition(EMPTY))
+//             .catch(error => transition(ERROR_DELETE, true));
+//     }
+
+//     return (
+//         <article className="appointment">
+//             <Header time={props.time} />
+//             {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+//             {mode === SHOW && (
+//                 <Show
+//                     student={props.interview.student}
+//                     interviewer={props.interview.interviewer}
+//                     onDelete={() => transition(CONFIRM)}
+//                     onEdit={() => transition(EDIT)}
+//                 />
+//             )}
+//             {mode === CREATE && (
+//                 <Form
+//                     interviewers={props.interviewers}
+//                     onCancel={() => back()}
+//                     onSave={save}
+//                 />
+//             )}
+//             {mode === SAVING && <Status message="Saving" />}
+//             {mode === DELETING && <Status message="Deleting" />}
+//             {mode === CONFIRM && (
+//                 <Confirm
+//                     message="Are you sure you would like to delete?"
+//                     onCancel={() => back()}
+//                     onConfirm={destroy}
+//                 />
+//             )}
+//             {mode === EDIT && (
+//                 <Form
+//                     name={props.interview.student}
+//                     interviewer={props.interview.interviewer.id}
+//                     interviewers={props.interviewers}
+//                     onCancel={() => back()}
+//                     onSave={save}
+//                 />
+//             )}
+//             {mode === ERROR_SAVE && (
+//                 <Error
+//                     message="Could not save appointment."
+//                     onClose={() => back()}
+//                 />
+//             )}
+//             {mode === ERROR_DELETE && (
+//                 <Error
+//                     message="Could not delete appointment." 
+//                     onClose={() => back()}  
+//                 />
+//             )}
+//         </article>
+//     );  
+// }
+export default function Appointment(props) {
+    return (
+        <article className="appointment"></article>
+    );
+}
--- stories/index.js
+++ stories/index.js
@@ -10,6 +10,8 @@ import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
+import Appointment from "components/Appointment/index.js";
+
 
 storiesOf("Button", module)
   .addParameters({
@@ -61,13 +63,13 @@ storiesOf("Button", module)
       backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
     })
     .add("Monday", () => (
-      <DayList days={days} day={"Monday"} setDay={action("setDay")} />
+      <DayList days={days} value={"Monday"} onChange={action("setDay")} />
     ))
     .add("Tuesday", () => (
-      <DayList days={days} day={"Tuesday"} setDay={action("setDay")} />
+      <DayList days={days} value={"Tuesday"} onChange={action("setDay")} />
     ))
     .add("Wednesday", () => (
-        <DayList days={days} day={"Wednesday"} setDay={action("setDay")} />
+        <DayList days={days} value={"Wednesday"} onChange={action("setDay")} />
     ));
 
     const interviewer = {
@@ -124,12 +126,19 @@ storiesOf("Button", module)
         .add("Selected", () => (
           <InterviewerList
             interviewers={interviewers}
-            interviewer={3}
+            value={3}
           />
         ))
         .add("Clickable", () => (
           <InterviewerList
             interviewers={interviewers}
-            setInterviewer={action("setInterviewer")}
+            onChange={action("setInterviewer")}
           />
-        ));ß
+        ));
+
+        storiesOf("Appointment", module) //Initiates Storybook and registers our Appointment component
+        .addParameters({
+          backgrounds: [{ name: "white", value: "#fff", default: true }]
+        }) // Provides the default background color for our component
+        .add("Appointment", () => <Appointment />)
+        .add("Appointment with Time", () => <Appointment time="12pm" />)
