32725befbeff6e7b93f034b68f8c28cc8f58b0b3
Elshan Magsudov <elshanmagsudov@Elshans-MacBook-Pro.local>
2023-03-06 18:03:32 +0400

    started combining states. implemented new state object for DayList component

--- src/components/Application.js
+++ src/components/Application.js
@@ -44,14 +44,19 @@ const appointments = {
   }
 };
 
+const setDay = day => setState({ ...state, day });
+const setDays = days => setState(prev => ({ ...prev, days }));
+
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
 
   useEffect(() => {
-  axios.get("/api/days").then((response) => {
-    setDays(response.data);
-  });
+  axios.get("/api/days").then(response => 
+    setDays(response.data));
   }, []);
 
   return (
@@ -65,9 +70,9 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            value={day}
-            onChange={setDay}
+            days={state.days}
+            value={state.day}
+            onChange={state.setDay}
           />
         </nav>
         <img
