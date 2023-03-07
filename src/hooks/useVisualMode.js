816589844d93f911a0e14390ed5b7c820ce41936
Elshan Magsudov <elshanmagsudov@Elshans-MacBook-Pro.local>
2023-03-07 12:30:02 +0400

    built custom hooks to handle transition and back, added tests, to ensure they are are functional without implementing them inside components installed react-hooks-testing-library and react-test-renderer

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+export default function useVisualMode (initial) {
+    const [mode, setMode] = useState(initial);
+    const [history, setHistory] = useState([initial]);
+    
+    function transition(newMode, replace=false) {
+        setMode(newMode);
+        setHistory( replace===true ? [...history.slice(0, -1), newMode] : [...history, newMode]);
+      }
+
+    function back() {
+        if (history.length > 1) {
+        setMode(history[history.length - 2]);
+        setHistory(
+            history.slice(0, -1));
+        }
+    }
+    
+      return { mode, transition, back, history };
+}
